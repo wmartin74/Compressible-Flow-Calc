e74497a18482b6be2cb8160d86de53173bf98ba0
wmartin74 <martin74@iastate.edu>
2025-08-13 15:40:13 -0500

    Update IsentropicResults

--- Scripts/IsentropicResults.js
+++ Scripts/IsentropicResults.js
@@ -1,6 +1,11 @@
 document.addEventListener("DOMContentLoaded", function () {
   results = JSON.parse(localStorage.getItem("IsentropicAnalysis"));
   console.log("Results Loaded")
-  
+
+  mach.innerHTML = results.Mach;
+  t0_t.innerHTML = results.T0_T;
+  p0_p.innerHTML = results.P0_P;
+  rho0_rho.innerHTML = results.Rho0_Rho;
+  a_as.innerHTML = results.A_As;
   
 });
