f68e9b9e8cc97da5ab8f2aef23f0011096c035ae
wmartin74 <martin74@iastate.edu>
2025-08-20 15:41:49 -0500

    added results and testing non linear solver

--- Scripts/IsentropicResults.js
+++ Scripts/IsentropicResults.js
@@ -8,5 +8,14 @@ document.addEventListener("DOMContentLoaded", function () {
   p0_p.innerHTML = results.P0_P;
   rho0_rho.innerHTML = results.Rho0_Rho;
   a_as.innerHTML = results.A_As;
+  mach_sub.innerHTML = results.Mach_sub;
+  t0t_sub.innerHTML = results.T0T_sub;
+  p0p_sub.innerHTML = results.P0P_sub;
+  rho0rho_sub.innerHTML = results.Rho0Rho_sub;
+  mach_super.innerHTML = results.Mach_super;
+  t0t_super.innerHTML = results.T0T_super;
+  p0p_super.innerHTML = results.P0P_super;
+  rho0rho_super.innerHTML = results.Rho0Rho_super;
+  
   
 });
