e732457a38acae462a2f4e549579427af6045ba3
wmartin74 <martin74@iastate.edu>
2025-08-28 12:51:43 -0500

    decmimal limiting

--- Scripts/IsentropicResults.js
+++ Scripts/IsentropicResults.js
@@ -3,7 +3,7 @@ document.addEventListener("DOMContentLoaded", function () {
   console.log("Results Loaded");
   console.log(results);
 
-  let element = [Mach, T0_T, P0_P, Rho0_Rho, A_As, Mach_sub, T0T_sub, P0P_sub, Rho0Rho_sub, Mach_super, T0T_super, P0P_super, Rho0Rho_super];
+  let element = ['Mach', 'T0_T', 'P0_P', 'Rho0_Rho', 'A_As', 'Mach_sub', 'T0T_sub', 'P0P_sub', 'Rho0Rho_sub', 'Mach_super', 'T0T_super', 'P0P_super', 'Rho0Rho_super'];
 
   for (let i = 0; i < element.length; i++) {
     if (results.element[i] !== null) {
