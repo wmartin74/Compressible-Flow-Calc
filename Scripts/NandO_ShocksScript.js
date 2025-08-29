c9c003575ac9fc36375549936a408c130115e121
wmartin74 <martin74@iastate.edu>
2025-08-29 10:46:45 -0500

    fixing function syntax

--- Scripts/NandO_ShocksScript.js
+++ Scripts/NandO_ShocksScript.js
@@ -20,24 +20,24 @@ function Press_jump(mach,p1) {
 
 //------ Normal Shock Functions ------
 function NormalShock(mach1,temp1,press1){
-  t0_t1 = temp_from_mach(mach1,gamma);
-  t01 = temp1*t0_t1;
+  const t0_t1 = temp_from_mach(mach1,gamma);
+  const t01 = temp1*t0_t1;
 
-  p0_p1 = press_from_temp(t0_t1);
-  p01 = press1*p0_p1;
+  const p0_p1 = press_from_temp(t0_t1);
+  const p01 = press1*p0_p1;
 
-  rho1 = (press1*p_convert)/(R*temp1);
+  const rho1 = (press1*p_convert)/(R*temp1);
 
-  mach2 = Math.pow((5+(mach1**2))/(7*(mach1**2)-1),0.5);
+  const mach2 = Math.pow((5+(mach1**2))/(7*(mach1**2)-1),0.5);
 
-  t0_t2 = temp_from_mach(mach2,gamma);
-  temp2 = t01/t0_t2;
+  const t0_t2 = temp_from_mach(mach2,gamma);
+  const temp2 = t01/t0_t2;
 
-  press2 = press_jump(mach1,press1);
-  p0_p2 = press_from_temp(t0_t2);
-  p02 = p0_p2*press2;
+  const press2 = press_jump(mach1,press1);
+  const p0_p2 = press_from_temp(t0_t2);
+  const p02 = p0_p2*press2;
 
-  rho2 = (press2*p_convert)/(R*temp2);
+  const rho2 = (press2*p_convert)/(R*temp2);
 
   return [mach2,temp2,press2,rho2,t01,p01,p02,rho1];
 }
