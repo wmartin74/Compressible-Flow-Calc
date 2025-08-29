7198781ae3a18a86be075ae3d5f71c2417a4d3b6
wmartin74 <martin74@iastate.edu>
2025-08-29 10:31:19 -0500

    normal shock prog

--- Scripts/NandO_ShocksScript.js
+++ Scripts/NandO_ShocksScript.js
@@ -1,9 +1,26 @@
 gamma = 1.4;
 R = 287.05;
+p_convert = 1.013e5;
+
+function temp_from_mach(mach,gamma) {
+  t0_t = (1+((gamma-1)/2)*mach**2);
+  return t0_t;
+}
+
+function press_from_temp(t0_t) {
+  p0_p = (t0_t)**(7/2);
+  return p0_p;
+}
 
 //------ Normal Shock Functions ------
 function NormalShock(mach1,temp1,press1){
-  
+  t0_t1 = temp_from_mach(mach1,gamma);
+  t01 = temp1*t0_t1;
+
+  p0_p1 = press_from_temp(t0_t1);
+  p01 = press1*p0_p1;
+
+
 }
 
 function runscript(event) {
