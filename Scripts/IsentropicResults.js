cf2db3eaeed85aecd6d5bc7ce80f560ff84f5106
wmartin74 <martin74@iastate.edu>
2025-08-28 19:59:12 -0500

    fixing result display

--- Scripts/IsentropicResults.js
+++ Scripts/IsentropicResults.js
@@ -6,14 +6,12 @@ document.addEventListener("DOMContentLoaded", function () {
   let element = ['Mach', 'T0_T', 'P0_P', 'Rho0_Rho', 'A_As', 'Mach_sub', 'T0T_sub', 'P0P_sub', 'Rho0Rho_sub', 'Mach_super', 'T0T_super', 'P0P_super', 'Rho0Rho_super'];
 
   for (let i = 0; i < element.length; i++) {
-    if (results.element[i] !== null) {
-      element[i].innerHTML = results.element[i].toFixed(4);
+    let key = document.getElementById(element[i]);
+    if (results[element[i]] !== null) {
+      key.innerHTML = results[element[i]].toFixed(4);
     } else {
       element[i].innerHTML = "N/A";
     }
   }
-
-
-  
   
 });
