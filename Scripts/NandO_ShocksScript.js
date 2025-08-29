27aa3aadd2a0ac50c5b9b72b1e3800727bdb5adb
wmartin74 <martin74@iastate.edu>
2025-08-29 08:42:47 -0500

    normal shock init

--- Scripts/NandO_ShocksScript.js
+++ Scripts/NandO_ShocksScript.js
@@ -1,3 +1,11 @@
+gamma = 1.4;
+R = 287.05;
+
+//------ Normal Shock Functions ------
+function NormalShock(mach1,temp1,press1){
+  
+}
+
 function runscript(event) {
   event.preventDefault();
   window.location.href = "NandO_ShocksResult.html";
