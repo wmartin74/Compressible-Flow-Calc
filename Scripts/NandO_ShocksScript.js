b45077066f7909f259a2119ac5514b3390694f7f
wmartin74 <martin74@iastate.edu>
2025-09-16 10:20:25 -0500

    finish oblique and added input query

--- Scripts/NandO_ShocksScript.js
+++ Scripts/NandO_ShocksScript.js
@@ -18,11 +18,6 @@ function Press_jump(mach,p1) {
   return p2;
 }
 
-// def obl_mach(gamma:float, Mn1:float, beta:float, theta:float):
-    // Mn2 = ((1+((gamma-1)/2)*(Mn1**2))/((gamma*(Mn1**2))-((gamma-1)/2)))**.5
-    // M2 = Mn2/(np.sin(np.deg2rad(beta-theta)))
-    // return M2
-
 function beta_solver(mach,theta,gamma) {
   mu = Math.asin(1/mach);
   function f(beta) {
@@ -61,11 +56,49 @@ function beta_solver(mach,theta,gamma) {
   return beta = NewtonSolver(theta, mu);
 }
 
-function ObliqueMach(gamma,Mn1,beta,Theta) {
-  const Mn2 = Math.pow((1+((gamma-1)/2)*(Mn1**2))/((gamma*(Mn1**2))-((gamma-1)/2)))**0.5;
-  const M2 = Mn2/(Math.sin(beta - Theta*(Math.PI/180)));
-  return M2;
+function ObliqueMach(gamma,mn1,beta,Theta) {
+  let mn2 = Math.pow((1+((gamma-1)/2)*(mn1**2))/((gamma*(mn1**2))-((gamma-1)/2)))**0.5;
+  let m2 = mn2/(Math.sin(beta - Theta*(Math.PI/180)));
+  return m2;
 }
+
+function density_ratio(mn1,gamma){
+  let rho21 = (6*mn1**2)/(5+mn1**2);
+  return rho21;
+}
+
+function pressure_ratio(mn1,gamma){
+  let p21 = 1+((2*gamma)/(gamma+1))*((mn1**2)-1);
+  return p21;
+}
+
+function temperature_ratio(rho21,p21){
+  let t21 = p21*(1/rho21);
+  return t21;
+}
+
+function temp_from_mach(mach,gamma) {
+  t0_t = (1+((gamma-1)/2)*mach**2);
+  return t0_t;
+}
+
+function press_from_temp(t0_t) {
+  p0_p = (t0_t)**(7/2);
+  return p0_p;
+}
+
+function Oblique_downstream(t1,p1,t21,p21,mach1,gamma,mach2){
+  let t2 = t1*t21;
+  let p2 = p1*p21;
+  let t0_t1 = temp_from_mach(mach1,gamma);
+  let t0_t2 = temp_from_mach(mach2,gamma);
+  let t01 = t1*t0_t1;
+  let p02 = p2*press_from_temp(t0_t2);
+  let p01 = p1*press_from_temp(t0_t1);
+
+  return [t2,p2,t01,p01,p02];
+}
+
 
 //------ Normal Shock Functions ------
 function NormalShock(mach1,temp1,press1){
@@ -92,15 +125,38 @@ function NormalShock(mach1,temp1,press1){
 }
 
 //------ Oblique Shock Functions ------
-function ObliqueShock(mach1,theta,temp1,press1) {
-  const beta = beta_solver(mach1,theta,gamma);
+function ObliqueShock(Mach1,theta,Temp1,Press1) {
+  const beta = beta_solver(Mach1,theta,gamma);
 
-  const NormalMach1 = mach1 * Math.sin(beta);
+  const NormalMach1 = Mach1 * Math.sin(beta);
 
-
+  const Mach2 = ObliqueMach(gamma,NormalMach1,beta,theta);
   
+  const Rho2_1 = density_ratio(NormalMach1,gamma);
+  const Press2_1 = pressure_ratio(NormalMach1,gamma);
+  const Temp2_1 = temperature_ratio(Rho2_1,Press2_1);
+
+  const [Temp2,Press2,Temp01,P01,P02] = Oblique_downstream(Temp1,Press1,Temp2_1,Press2_1,Mach1,gamma,Mach2);
+  const Rho1 = (Press1*p_convert)/(R*Temp1);
+  const Rho2 = (Press2*p_convert)/(R*Temp2);
+
+  return [beta,Mach2,Temp2,Press2,Rho1,Rho2,Temp01,P01,P02];
 }
 
+function pulldata() {
+    try {
+        const ShockSelection = document.querySelector('input[name="shock"]:checked').value;
+        const Mach1 = parseFloat(document.getElementById("mach").value);
+        const Temp1 = parseFloat(document.getElementById('temp').value);
+        const Press1 = parseFloat(document.getElementById('press').value);
+        const Theta = parseFloat(document.getElementById('angle').value);
+        return [ShockSelection, Mach1, Temp1, Press1, Theta];
+    } catch (error) {
+        console.error("Error parsing input:", error);
+        window.alert("Please enter a select an option and enter a valid ratio");
+        return null;
+    }
+}
 
 
 function runscript(event) {
